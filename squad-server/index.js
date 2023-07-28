c2680ee8681e369525a4545eddd30bf1e8986b14
Skillet <kilerskyman@gmail.com>
2023-07-28 19:40:11 -0400

    Update index.js

--- squad-server/index.js
+++ squad-server/index.js
@@ -620,7 +620,7 @@ export default class SquadServer extends EventEmitter {
   mapLayer(layid) {
     layid = layid.replace(/[^a-z_\d]/gi, '');
     const gl =
-      /^((?<mod>[A-Z]+)_)?(?<level>[A-Za-z]+)_((?<gamemode>[A-Za-z]+)(_|$))?((?<version>[vV][0-9]+)(_|$))?((?<team1>[a-zA-Z0-9]+)[-v](?<team2>[a-zA-Z0-9]+))?/.exec(
+      /^((?<mod>[A-Z]+)_)?(?<level>[A-Za-z]+)_((?<gamemode>[A-Za-z]+)(_|$))?((?<version>[vV][0-9]+)(_|$))?((?<team1>[a-zA-Z0-9]+)[-v](?<team2>[a-zA-Z0-9]+))?/gm.exec(
         layid
       )?.groups;
     if (!gl) return;
